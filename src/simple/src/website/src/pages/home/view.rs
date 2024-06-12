d6ad1f79a62c1753ab66bd546b7e185eb0061d2e
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-12 15:04:32 -0700

    Update view.rs

--- src/simple/src/website/src/pages/home/view.rs
+++ src/simple/src/website/src/pages/home/view.rs
@@ -3,21 +3,6 @@ use leptos_meta::*;
 use leptos_router::*;
 
 #[component]
-pub fn App() -> impl IntoView {
-    provide_meta_context();
-
-    view! {
-        <Stylesheet id="leptos" href="/pkg/website.css"/>
-        <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
-        <Router>
-            <Routes>
-                <Route path="" view=  move || view! { <Home/> }/>
-            </Routes>
-        </Router>
-    }
-}
-
-#[component]
 fn Home() -> impl IntoView {
     let (value, set_value) = create_signal(0);
 
