b7326dae41bbc73cacbe4dd806a31326bbd7623a
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-12 00:56:42 -0700

    Update main.rs

--- src/simple/src/server/src/main.rs
+++ src/simple/src/server/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
 }
