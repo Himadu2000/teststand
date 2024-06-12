3fb1f4247ab70e6e443eeaa54cddec08ce231a24
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-12 15:06:14 -0700

    Update mod.rs

---
+++ src/simple/src/website/src/pages/mod.rs
@@ -0,0 +1,20 @@
+mod home;
+
+use leptos::*;
+use leptos_meta::*;
+use leptos_router::*;
+
+#[component]
+pub fn App() -> impl IntoView {
+    provide_meta_context();
+
+    view! {
+        <Stylesheet id="leptos" href="/pkg/website.css"/>
+        <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
+        <Router>
+            <Routes>
+                <Route path="" view=  move || view! { <Home/> }/>
+            </Routes>
+        </Router>
+    }
+}
