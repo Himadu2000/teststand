cb75e60467df6a133d9a64745d9dae3e36ae01f6
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-12 15:32:48 -0700

    Update mod.rs

--- src/simple/src/website/src/pages/home/mod.rs
+++ src/simple/src/website/src/pages/home/mod.rs
@@ -1,1 +1,15 @@
 mod view;
+
+use leptos::*;
+use leptos_meta::*;
+use view::View;
+
+#[component]
+pub fn Home() -> impl IntoView {
+    let (value, set_value) = create_signal(0);
+
+    // thanks to https://tailwindcomponents.com/component/blue-buttons-example for the showcase layout
+    view! {
+        <View />
+    }
+}
