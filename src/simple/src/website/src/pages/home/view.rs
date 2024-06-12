b0365fdd0711edc143322fb483d19608ba22b1c5
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-12 15:33:32 -0700

    Update view.rs

--- src/simple/src/website/src/pages/home/view.rs
+++ src/simple/src/website/src/pages/home/view.rs
@@ -3,9 +3,6 @@ use leptos_meta::*;
 
 #[component]
 pub fn View() -> impl IntoView {
-    let (value, set_value) = create_signal(0);
-
-    // thanks to https://tailwindcomponents.com/component/blue-buttons-example for the showcase layout
     view! {
         <Title text="Home"/>
         <main>
