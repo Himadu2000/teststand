a03f0a70123d85725e3e0cb0eea4d5cb4e4cec22
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-12 15:34:49 -0700

    Update mod.rs

--- src/simple/src/website/src/pages/home/mod.rs
+++ src/simple/src/website/src/pages/home/mod.rs
@@ -8,7 +8,16 @@ pub fn Home() -> impl IntoView {
     let (value, set_value) = create_signal(0);
 
     // thanks to https://tailwindcomponents.com/component/blue-buttons-example for the showcase layout
+
+    fn add() {
+        set_value.update(|value| *value += 1)
+    }
+
+    fn sub() {
+        set_value.update(|value| *value -= 1)
+    }
+
     view! {
-        <View />
+        <View add sub />
     }
 }
