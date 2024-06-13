3b3d1b0bb6bf3829c8235c9cd2e9db21f6fdce45
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-12 19:57:16 -0700

    Update view.rs

--- src/simple/src/website/src/pages/home/view.rs
+++ src/simple/src/website/src/pages/home/view.rs
@@ -9,13 +9,13 @@ pub fn View(data: Data) -> impl IntoView {
         <main>
             <div class="bg-gradient-to-tl from-blue-800 to-blue-500 text-white font-mono flex flex-col min-h-screen">
                 <div class="flex flex-row-reverse flex-wrap m-auto">
-                    <button on:click=move |_| set_value.update(|value| *value += 1) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                    <button on:click=events.0 class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
                         "+"
                     </button>
                     <button class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-800 border-blue-900 text-white">
-                        {value}
+                        {data.value}
                     </button>
-                    <button on:click=move |_| set_value.update(|value| *value -= 1) class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
+                    <button on:click=events.1 class="rounded px-3 py-2 m-1 border-b-4 border-l-2 shadow-lg bg-blue-700 border-blue-800 text-white">
                         "-"
                     </button>
                 </div>
