a670e3bef2f7c02064cfea3c390d8aeb91d66756
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-18 02:28:29 -0700

    Update build.rs

--- src/simple/src/website/build.rs
+++ src/simple/src/website/build.rs
@@ -2,7 +2,7 @@ use cynic_codegen::register_schema;
 
 fn main() {
     register_schema("schema")
-        .from_sdl_file("schema.graphql")
+        .from_sdl_file("src/schema.graphql")
         .unwrap()
         .as_default()
         .unwrap();
