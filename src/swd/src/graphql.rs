71a55448aa56d959299657e3f48f5e7ffb65dcc7
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-19 18:21:40 -0700

    Update graphql.rs

--- src/swd/src/graphql.rs
+++ src/swd/src/graphql.rs
@@ -1,5 +1,4 @@
 use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
-use async_graphql_rocket::{GraphQLQuery, GraphQLRequest, GraphQLResponse};
 use rocket::{get, post, response::content::RawHtml, routes, Route, State};
 
 pub type GraphqlSchema<Query, Mutation = EmptyMutation, Subscription = EmptySubscription> =
@@ -8,18 +7,4 @@ pub type GraphqlSchema<Query, Mutation = EmptyMutation, Subscription = EmptySubs
 #[get("/")]
 pub fn graphiql() -> RawHtml<String> {
     RawHtml(GraphiQLSource::build().endpoint("/graphql").finish())
-}
-
-#[get("/?<query..>")]
-async fn graphql_query(schema: &GraphqlSchema, query: GraphQLQuery) -> GraphQLResponse {
-    query.execute(schema.inner()).await
-}
-
-#[post("/", data = "<request>", format = "application/json")]
-async fn graphql_request(schema: &GraphqlSchema, request: GraphQLRequest) -> GraphQLResponse {
-    request.execute(schema.inner()).await
-}
-
-pub fn graphql() -> impl Into<Vec<Route>> {
-    routes![graphiql, graphql_query, graphql_request]
 }
