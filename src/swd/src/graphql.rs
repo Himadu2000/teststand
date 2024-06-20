11c4ddb89c32b5bf46398dbaae1010e628e6a56f
Himadu2000 <49078336+Himadu2000@users.noreply.github.com>
2024-06-19 18:22:05 -0700

    Update graphql.rs

--- src/swd/src/graphql.rs
+++ src/swd/src/graphql.rs
@@ -1,5 +1,5 @@
 use async_graphql::{http::GraphiQLSource, EmptyMutation, EmptySubscription, Schema};
-use rocket::{get, post, response::content::RawHtml, routes, Route, State};
+use rocket::{get, response::content::RawHtml, State};
 
 pub type GraphqlSchema<Query, Mutation = EmptyMutation, Subscription = EmptySubscription> =
     State<Schema<Query, Mutation, Subscription>>;
